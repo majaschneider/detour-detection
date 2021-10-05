eee3ff73516530017d06c47306cead205835b5a1
Lukas Gehrke <lg58weky@studserv.uni-leipzig.de>
2021-10-05 10:57:46 +0200

    Add function to select points from a route given a temporal distance.

---
+++ detour_metric.py
@@ -0,0 +1,63 @@
+"""This module detects detours to measure the privacy risk inherent to spatio-temporal trajectories containing stops.
+"""
+
+from geodata import route as rt
+import pandas as pd
+
+
+def select_samples(route, temporal_distance, start_timestamp=None):
+    """
+    Samples points from a given route with a certain temporal distance. Sampling starts at the first route point or, if
+    provided, at start_timestamp. The route must contain timestamps. The function assumes that the temporal distances
+    between points in the route are equal. Dealing with irregular timestamps will be addressed later.
+
+    Parameter
+    ---------
+    route : geodata.route.Route
+        A route object containing trajectory data represented as geodata.point_t.PointT. If the parameter route is empty
+        the returned route is empty as well. The points in the route are assumed to be sorted according to timestamp. In
+         other words: the oldest timestamp is at the first index.
+    temporal_distance : pandas.Timedelta
+        The temporal distance in seconds between sampled points.
+    start_timestamp : pandas.Timestamp
+        An optional timestamp that marks the starting point for selection of samples. If None, sampling starts at the
+        first route point.
+    Returns
+    -------
+    sampled_points : geodata.route.Route
+        A route containing the sampled points according to the temporal distance.
+    """
+    if not(isinstance(route, rt.Route) and route.has_timestamps()):
+        raise(ValueError(
+            "Wrong value for param route. Make sure it is of route.Route and its points are have timestamps."
+         ))
+
+    if not(isinstance(temporal_distance, pd.Timedelta)) or temporal_distance.seconds < 0:
+        raise(ValueError(
+            "Wrong value for param temporal distance. Make sure it is of type pandas.Timedelta and larger than zero."
+        ))
+
+    sampled_points = rt.Route()
+    current_idx = 0
+
+    # If start_timestamp is given, iterate over the route until it is reached
+    if start_timestamp is not None:
+        if not isinstance(start_timestamp, pd.Timestamp):
+            raise ValueError("Wrong value for start timestamp. Make sure it is of type pandas.Timestamp.")
+        for point in route:
+            if point.timestamp < start_timestamp:
+                current_idx += 1
+
+    # Add first point before iterating.
+    if current_idx < len(route):
+        sampled_points.append(route[current_idx])
+
+    # Iterate over the rest of the route and check whether current and next point's timestamps are temporal_distance
+    # apart
+    for next_idx in range(current_idx+1, len(route)):
+        next_timestamp = route[next_idx].timestamp
+        if (next_timestamp-route[current_idx].timestamp) >= temporal_distance:
+            sampled_points.append(route[next_idx])
+            current_idx = next_idx
+
+    return sampled_points
---
+++ tests/test_detour_metric.py
@@ -0,0 +1,102 @@
+import unittest
+
+import datetime
+import pandas as pd
+from geodatasets import de4l
+from geodata import route as rt
+
+import detour_metric
+
+
+class TestMetric(unittest.TestCase):
+
+    def test_select_samples(self):
+
+        # Manually define the timestamps that should be in the test dataset
+        timestamps = [
+            pd.Timestamp("2021-02-22T10:31:33.000Z"),
+            pd.Timestamp("2021-02-22T10:31:52.000Z"),
+            pd.Timestamp("2021-02-22T10:32:07.000Z"),
+            pd.Timestamp("2021-02-22T10:32:17.000Z"),
+            pd.Timestamp("2021-02-22T10:32:40.000Z"),
+            pd.Timestamp("2021-02-22T10:32:41.000Z"),
+            pd.Timestamp("2021-02-22T10:32:50.000Z"),
+            pd.Timestamp("2021-02-22T10:33:00.000Z"),
+            pd.Timestamp("2021-02-22T10:33:23.000Z"),
+            pd.Timestamp("2021-02-22T10:33:26.000Z"),
+            pd.Timestamp("2021-02-22T10:33:27.000Z")
+        ]
+
+        # Define desired timedeltas for the iteration
+        timedeltas = [
+            pd.Timedelta(value=0, unit="sec"),
+            pd.Timedelta(value=1, unit="sec"),
+            pd.Timedelta(value=2, unit="sec"),
+            pd.Timedelta(value=5, unit="sec"),
+            pd.Timedelta(value=15, unit="sec"),
+            pd.Timedelta(value=30, unit="sec"),
+            pd.Timedelta(value=1, unit="min"),
+            pd.Timedelta(value=2, unit="min"),
+            pd.Timedelta(value=1500, unit="millis"),
+        ]
+
+        dataset = de4l.De4lSensorDataset.create_from_json("../resources/test-dataset.json", route_len=11)
+
+        # Get a route from the dataset
+        route = dataset[0]["route_with_timestamps"]
+
+        # Define different timestamps to test sampling with start timestamp
+        start_timestamp_middle = pd.Timestamp("2021-02-22T10:32:41.000Z")
+        start_timestamp_late = pd.Timestamp("2021-02-22T10:33:27.000Z")
+        start_timestamp_too_late = pd.Timestamp("2021-02-22T10:33:28.000Z")
+
+        # Test different parameter setups that contain temporal distance, start timestamp and indices of expected
+        # sampled points and validate the sampled points
+        for temporal_distance, start_timestamp, expected_indices in [
+            # Without start timestamp
+            (timedeltas[0], None, list(range(11))),
+            (timedeltas[1], None, list(range(11))),
+            (timedeltas[2], None, [0, 1, 2, 3, 4, 6, 7, 8, 9]),
+            (timedeltas[3], None, [0, 1, 2, 3, 4, 6, 7, 8]),
+            (timedeltas[4], None, [0, 1, 2, 4, 7, 8]),
+            (timedeltas[5], None, [0, 2, 4, 8]),
+            (timedeltas[6], None, [0, 4]),
+            (timedeltas[7], None, [0]),
+            (timedeltas[8], None, [0, 1, 2, 3, 4, 6, 7, 8, 9]),
+            # With start timestamp in the middle of the timestamps to sample
+            (timedeltas[1], start_timestamp_middle, list(range(5, 11))),
+            (timedeltas[2], start_timestamp_middle, [5, 6, 7, 8, 9]),
+            (timedeltas[3], start_timestamp_middle, [5, 6, 7, 8]),
+            (timedeltas[4], start_timestamp_middle, [5, 7, 8]),
+            (timedeltas[5], start_timestamp_middle, [5, 8]),
+            (timedeltas[6], start_timestamp_middle, [5]),
+            (timedeltas[7], start_timestamp_middle, [5]),
+            # With start timestamp at the end of the timestamps to sample
+            (timedeltas[3], start_timestamp_late, [10]),
+            # With start timestamp later than the timestamps to sample
+            (timedeltas[3], start_timestamp_too_late, []),
+        ]:
+            sampled_points = detour_metric.select_samples(route, temporal_distance, start_timestamp)
+            expected_timestamps = [timestamps[idx] for idx in expected_indices]
+            sampled_timestamps = [point.timestamp for point in sampled_points]
+            self.assertEqual(expected_timestamps, sampled_timestamps)
+
+        # Test parameters that should lead to an error
+        with self.assertRaises(ValueError):
+            detour_metric.select_samples(route, "1")
+            detour_metric.select_samples(route, 1)
+            detour_metric.select_samples(route, pd.Timedelta(value=1, unit="sec"), "2021-02-22T10:32:41.000Z")
+            detour_metric.select_samples(route, pd.Timedelta(value=1, unit="sec"), 1613989893.0)
+            # Disallow timestamps or timedeltas of the datetime module
+            detour_metric.select_samples(route, datetime.timedelta(seconds=5), timestamps([0]))
+            detour_metric.select_samples(
+                route, pd.Timedelta(value=1, unit="sec"), datetime.datetime.fromtimestamp("2021-02-22T10:32:41.000Z")
+            )
+            # Test if passing an empty route leads to an error
+            detour_metric.select_samples(rt.Route(), temporal_distance=pd.Timedelta(value=1, unit="seconds"))
+            # Test if passing a wrong type for route leads to an error
+            detour_metric.select_samples([[1, 1]], temporal_distance=pd.Timedelta(value=1, unit="seconds"))
+
+
+if __name__ == "__main__":
+    unittest.main()
